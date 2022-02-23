756d6db616eb1ba35754a7be5cc1c19a865d5b5e
Juan José Aristizábal <juanesaristisabal@gmail.com>
2022-02-22 19:13:35 -0500

    security fixes

--- mysite/settings.py
+++ mysite/settings.py
@@ -19,8 +19,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
+import os
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@esxz!o5q2i!g!=5$07z&3h+=p1t4_bsplpjlin_2gcjscvke7'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -55,8 +57,6 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'mysite.urls'
-
-import os
 
 TEMPLATES = [
     {
